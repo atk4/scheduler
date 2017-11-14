169e9bf93b9e11760cd82ff76a7543bf6468c370
Alex00Sam <alsa00@mail.ru>
2017-11-14 19:51:05 +0200

    date fix

--- application.php
+++ application.php
@@ -6,26 +6,30 @@
   $header = $app->add(['Header',$name.' ('.$phone.')']);
 //$timeslot = $day->ref('Timeslot');
   $menu = $app->add('Menu');
-  for($i=7;$i<=14;$i++) {
+  for($i=14;$i<=21;$i++) {
     $date_day=$date_day+1;
-    if (isset($check)){
-      if($date_month==[1,3,5,7,9,11]){
-        if($date_day+$i>31){
+    if (!isset($check)){
+      if(($date_month==1) and ($date_month==3)and ($date_month==5) and ($date_month==7)and ($date_month==9)and ($date_month==11)){
+        if($date_day>31){
           $date_month = $date_month+1;
           $check=TRUE;
+          $date_day = $date_day-31;
         }
       } else{
-          if($date_month<>28){
-            if($date_day+$i>28){
-              $date_month = $date_month+1;
+          if($date_month==2){
+            if($date_day>28){
+              $date_day = $date_day-28;
+              $date_month=$date_month+1;
               $check=TRUE;
             }
           }else{
-            $date_month = $date_month+1;
-            $check=TRUE;
+            if($date_day>30){
+              $date_day = $date_day-30;
+              $date_month=$date_month+1;
+              $check=TRUE;
+            }
           }
         }
     }
-
     $menuitem= $menu->addMenu($date_day.'.'.$date_month);
   }
