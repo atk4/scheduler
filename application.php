a2b77097c36fb0816e5bcd0cfde8369a46942923
Alex00Sam <alsa00@mail.ru>
2017-11-14 18:04:56 +0200

    dates

--- application.php
+++ application.php
@@ -4,3 +4,28 @@
   $name = $teacher['name'];
   $phone = $teacher['contact_phone'];
   $header = $app->add(['Header',$name.' ('.$phone.')']);
+//$timeslot = $day->ref('Timeslot');
+  $menu = $app->add('Menu');
+  for($i=7;$i<=14;$i++) {
+    $date_day=$date_day+1;
+    if (isset($check)){
+      if($date_month==[1,3,5,7,9,11]){
+        if($date_day+$i>31){
+          $date_month = $date_month+1;
+          $check=TRUE;
+        }
+      } else{
+          if($date_month<>28){
+            if($date_day+$i>28){
+              $date_month = $date_month+1;
+              $check=TRUE;
+            }
+          }else{
+            $date_month = $date_month+1;
+            $check=TRUE;
+          }
+        }
+    }
+
+    $menuitem= $menu->addMenu($date_day.'.'.$date_month);
+  }
--- init.php
+++ init.php
@@ -1,7 +1,10 @@
 <?php
+date_default_timezone_set('UTC');
 require 'vendor/autoload.php';
 $app = new App('public');
 $subject = new Model\Subject($app->db);
 $teacher = new Model\Teacher($app->db);
 $day = new Model\Day($app->db);
 $timeslot = new Model\Timeslot($app->db);
+$date_day = date('d');
+$date_month = date('m');
