c60744e1eae67822fcd698525c1cf652a284082f
Artur Konevnikov <artik292@gmail.com>
2018-01-08 21:14:25 +0200

    Working admin.php
    Debaging ready

--- admin.php
+++ admin.php
@@ -6,16 +6,16 @@ $app = new App('admin');
 
 date_default_timezone_set('UTC');
 
+$app->stickyget('check');
+
 if (isset($_GET['check'])) {
   $check = $_GET['check'];
+  $crud = $app->add('CRUD');
   if ($check == 'lessens') {
-    $crud = $app->add('CRUD');
     $crud->setModel(new Model\Subject($app->db));
   } elseif ($check == 'teachers') {
-    $crud = $app->add('CRUD');
     $crud->setModel(new Model\Teacher($app->db));
   } elseif ($check == 'list') {
-    $crud = $app->add('CRUD');
     $crud->setModel(new Model\Vecaki($app->db));
   }
 
