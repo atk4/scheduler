1e30983683ae43710239fa4e5ebb035ff40c20ff
Artur Konevnikov <artik292@gmail.com>
2018-01-08 19:04:57 +0200

    Admin update

--- admin.php
+++ admin.php
@@ -6,15 +6,17 @@ $app = new App('admin');
 
 date_default_timezone_set('UTC');
 
-$admin = $_GET['admin'];
+if (isset($_GET['check'])) {
+  $check = $_GET['check'];
+  if ($check == 'lessens') {
+    $crud = $app->add('CRUD');
+    $crud->setModel(new Model\Subject($app->db));
+  } elseif ($check == 'teachers') {
+    $crud = $app->add('CRUD');
+    $crud->setModel(new Model\Teacher($app->db));
+  } elseif ($check == 'list') {
+    $crud = $app->add('CRUD');
+    $crud->setModel(new Model\Vecaki($app->db));
+  }
 
-if ($admin == 'lessens') {
-  $crud = $app->add('CRUD');
-  $crud->setModel(new Model\Subject($app->db));
-} elseif ($admin == 'teachers') {
-  $crud = $app->add('CRUD');
-  $crud->setModel(new Model\Teacher($app->db));
-} elseif ($admin == 'list') {
-  $crud = $app->add('CRUD');
-  $crud->setModel(new Model\Vecaki($app->db));
 }
--- src/App.php
+++ src/App.php
@@ -14,9 +14,9 @@ class App extends \atk4\ui\App {
             $this->initLayout('Admin');
             $this->layout->leftMenu->addItem(['Galvenā lapa', 'icon'=>'home'], ['index']);
             //$this->layout->leftMenu->addItem(['Admin', 'icon'=>'dashboard'], ['admin']);
-            $this->layout->leftMenu->addItem(['Priekšmeti', 'icon'=>'book'], ['admin','admin'=>'lessens']);
-            $this->layout->leftMenu->addItem(['Skolotāji', 'icon'=>'users'], ['admin','admin'=>'teachers']);
-            $this->layout->leftMenu->addItem(['Ieraksti', 'icon'=>'unordered list'], ['admin','admin'=>'list']);
+            $this->layout->leftMenu->addItem(['Priekšmeti', 'icon'=>'book'], ['admin','check'=>'lessens']);
+            $this->layout->leftMenu->addItem(['Skolotāji', 'icon'=>'users'], ['admin','check'=>'teachers']);
+            $this->layout->leftMenu->addItem(['Ieraksti', 'icon'=>'unordered list'], ['admin','check'=>'list']);
         }
         $this->db = \atk4\data\Persistence::connect('mysql://MySite:12345@localhost/scheduler');
         //$this->db = new	\atk4\data\Persistence_SQL('mysql:dbname=scheduler;host=localhost','root','');
