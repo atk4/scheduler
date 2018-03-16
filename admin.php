d37949fea7f81690c5a8a98f48aeea9719669553
example <example356e@gmail.com>
2018-03-16 17:50:32 +0200

    Big update for admin

--- admin.php
+++ admin.php
@@ -4,23 +4,33 @@ require 'vendor/autoload.php';
 
 $app = new App('admin');
 
-date_default_timezone_set('UTC');
+session_start();
 
-$app->stickyget('check');
+If (isset($_SESSION['admin_access'])) {
+  If (($_SESSION['admin_access']) == 'tkvbk0/0ilyvmamy') {
+    date_default_timezone_set('UTC');
 
-if (isset($_GET['check'])) {
-  $check = $_GET['check'];
-  $crud = $app->add('CRUD');
-  if ($check == 'lessens') {
-    $crud->setModel(new Model\Subject($app->db));
-    $crud->addQuickSearch(['name']);
-  } elseif ($check == 'teachers') {
-    $crud->setModel(new Model\Teacher($app->db));
-    $crud->addQuickSearch(['name','cabinet']);
+    $app->stickyget('check');
+
+    if (isset($_GET['check'])) {
+      $check = $_GET['check'];
+      $crud = $app->add('CRUD');
+      if ($check == 'lessens') {
+        $crud->setModel(new Model\Subject($app->db));
+        $crud->addQuickSearch(['name']);
+      } elseif ($check == 'teachers') {
+        $crud->setModel(new Model\Teacher($app->db));
+        $crud->addQuickSearch(['name','cabinet']);
+      }
+
+    } else {
+      $crud = $app->add('CRUD');
+      $crud->setModel(new Model\Vecaki($app->db));
+      $crud->addQuickSearch(['student_name','parent_name','contact_phone','time']);
+    }
+  } else {
+    header('Location: index.php');
   }
-
 } else {
-  $crud = $app->add('CRUD');
-  $crud->setModel(new Model\Vecaki($app->db));
-  $crud->addQuickSearch(['student_name','parent_name','contact_phone','time']);
+  header('Location: index.php');
 }
